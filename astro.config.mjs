2957af6de093e97fc874e832c23eb284e67ba53a
germes2013 <salimsalima101@gmail.com>
2022-12-21 15:52:48 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import svelte from "@astrojs/svelte";
 
 export default defineConfig({
   //замените на ваш домен
-  site: "https://смотри-read.me",
+  site: "https://sweet-kitten-dcaa13.netlify.app",
   integrations: [tailwind(), svelte()],
 });
